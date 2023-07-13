67922068fbc788d3af6caa04ffa517dd71823090
dagmara <dagmara.gabriela.stach@gmail.com>
2023-07-13 16:18:34 +0200

    searching throught googlde drive folders

--- outPutFolder.js
+++ outPutFolder.js
@@ -1,9 +1,14 @@
 import fs from "fs"
-const folderPath = "/Users/dagmara/dagmara.gabriela.stach@gmail.com - Google Drive/Mój dysk/docProdukcja"
+const folderPath = "/Users/dagmara/dagmara.gabriela.stach@gmail.com - Google Drive/Mój dysk/docProdukcja/stanowisko_kontroli/RB0508"
 
 function outPutFolder(){
-fs.readdirSync(folderPath).forEach((file)=>{
-    console.log(file)
-})
-}
+fs.readdirSync(folderPath).filter(fn =>{
+    if(!fn.startsWith(".")){
+        if (fn.endsWith('') && (!fn.endsWith('.pdf')))
+            console.log("machine", fn)}
+        if (fn.endsWith('.pdf')){
+            console.log('instruction', fn)
+        }
+        }
+)}
 outPutFolder()
