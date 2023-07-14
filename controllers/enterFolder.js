8cfe90837967ca04cbbd9daf8167a7c82d7ab483
dagmara <dagmara.gabriela.stach@gmail.com>
2023-07-14 16:06:01 +0200

    EnterFolder endPoint - request for sub folders

--- controllers/enterFolder.js
+++ controllers/enterFolder.js
@@ -1,6 +1,6 @@
 import fs from "fs"
 
-export default function handleEnterFolder(req,res) {
+export default function enterFolder(req,res) {
     console.log("handleEnterFolder RES", req.query.entfn)
     const rootPath = "/Users/dagmara/dagmara.gabriela.stach@gmail.com - Google Drive/Mój dysk/docProdukcja"
     const folderPath = rootPath + "/" + req.query.entfn;
@@ -8,10 +8,20 @@ export default function handleEnterFolder(req,res) {
     fs.readdirSync(folderPath).filter(fn =>{
     if(!fn.startsWith(".")){
         if (fn.endsWith('') && (!fn.endsWith('.pdf')))
-            console.log("machine", fn)}
+            console.log("machine", fn)
         if (fn.endsWith('.pdf')){
             console.log('instruction', fn)
+    }
         }
+
+   /*     if(!fn.startsWith(".")){
+            if (fn.endsWith('') && (!fn.endsWith('.pdf')))
+                console.log("machine", fn)}
+        if (fn.endsWith('.pdf')){
+            console.log('instruction', fn)
+        }*/
+
+
         }
 )
 }
