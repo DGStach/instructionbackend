e8e70564cdcbec09dde3d04f4b5acc2f070b0a32
dagmara <dagmara.gabriela.stach@gmail.com>
2023-07-15 13:46:54 +0200

    tests enterFolder 1

--- tests/enterFolder.test.js
+++ tests/enterFolder.test.js
@@ -3,3 +3,4 @@ import enterFolder from "../controllers/enterFolder.js"
 test("opis testu", ()=>{
     expect(enterFolder('"/Users/dagmara/dagmara.gabriela.stach@gmail.com - Google Drive/Mój dysk/docProdukcja/stanowiskoMontażu2')).toBe('Hi');
 })
+
