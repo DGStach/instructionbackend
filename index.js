0c24bcf347cda6cf0d3449f59d74bbb3a1e1edeb
dagmara <dagmara.gabriela.stach@gmail.com>
2023-07-18 14:34:45 +0200

    Google Cloud Platform

--- index.js
+++ index.js
@@ -1,13 +1,102 @@
-// index.js
-import fetch from "node-fetch";
-const { writeFile } = require('fs').promises;
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
+import "./client_secret_560046063698-q4ev26rhqsv2f8a1snv58n5pvs01qo8o.apps.googleusercontent.com.json"
 
-const url =
-    'https://test-videos.co.uk/vids/bigbuckbunny/mp4/h264/360/Big_Buck_Bunny_360_10s_1MB.mp4';
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/script.projects'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
-(async () => {
-    const response = await fetch(url);
-    const buffer = await response.buffer();
-    await writeFile('test.mp4', buffer);
-    console.log('Done!');
-})();
+/**
+ * Reads previously authorized credentials from the save file.
+ *
+ * @return {Promise<OAuth2Client|null>}
+ */
+async function loadSavedCredentialsIfExist() {
+    try {
+        const content = await fs.readFile(TOKEN_PATH);
+        const credentials = JSON.parse(content);
+        return google.auth.fromJSON(credentials);
+    } catch (err) {
+        return null;
+    }
+}
+
+/**
+ * Serializes credentials to a file comptible with GoogleAUth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+    const content = await fs.readFile(CREDENTIALS_PATH);
+    const keys = JSON.parse(content);
+    const key = keys.installed || keys.web;
+    const payload = JSON.stringify({
+        type: 'authorized_user',
+        client_id: key.client_id,
+        client_secret: key.client_secret,
+        refresh_token: client.credentials.refresh_token,
+    });
+    await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+    let client = await loadSavedCredentialsIfExist();
+    if (client) {
+        return client;
+    }
+    client = await authenticate({
+        scopes: SCOPES,
+        keyfilePath: CREDENTIALS_PATH,
+    });
+    if (client.credentials) {
+        await saveCredentials(client);
+    }
+    return client;
+}
+
+/**
+ * Creates a new script project, upload a file, and log the script's URL.
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+async function callAppsScript(auth) {
+    const script = google.script({version: 'v1', auth});
+    let res = await script.projects.create({
+        resource: {
+            title: 'My Script',
+        },
+    });
+    res = await script.projects.updateContent({
+        scriptId: res.data.scriptId,
+        auth,
+        resource: {
+            files: [
+                {
+                    name: 'hello',
+                    type: 'SERVER_JS',
+                    source: 'function helloWorld() {\n  console.log("Hello, world!");\n}',
+                },
+                {
+                    name: 'appsscript',
+                    type: 'JSON',
+                    source:
+                        '{"timeZone":"America/New_York","exceptionLogging":' + '"CLOUD"}',
+                },
+            ],
+        },
+    });
+    console.log(`https://script.google.com/d/${res.data.scriptId}/edit`);
+}
+
+authorize().then(callAppsScript).catch(console.error);
