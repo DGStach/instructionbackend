c0e95e951bac79d0b1a7a517594e059cecddfbc9
dagmara <dagmara.gabriela.stach@gmail.com>
2023-07-19 14:38:41 +0200

    Google Cloud Platform - Access Google Drive Using a Node Server - getUserInfo & readDrive

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/getAuthURL', (req, res) => {
     const authUrl = oAuth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: SCOPE,
-        prompt: 'force'
+        prompt:'consent'
     });
     console.log(authUrl);
     return res.send(authUrl);
@@ -49,6 +49,41 @@ app.post('/getToken', (req, res) => {
     });
 });
 
+app.post('/getUserInfo', (req, res) => {
+    if (req.body.token == null) return res.status(400).send('Token not found');
+    oAuth2Client.setCredentials(req.body.token);
+    const oauth2 = google.oauth2({ version: 'v2', auth: oAuth2Client });
+
+    oauth2.userinfo.get((err, response) => {
+        if (err) res.status(400).send(err);
+        console.log(response.data);
+        res.send(response.data);
+    })
+});
+
+app.post('/readDrive', (req, res) => {
+    if (req.body.token == null) return res.status(400).send('Token not found');
+    oAuth2Client.setCredentials(req.body.token);
+    const drive = google.drive({ version: 'v3', auth: oAuth2Client });
+    drive.files.list({
+        pageSize: 10,
+    }, (err, response) => {
+        if (err) {
+            console.log('The API returned an error: ' + err);
+            return res.status(400).send(err);
+        }
+        const files = response.data.files;
+        if (files.length) {
+            console.log('Files:');
+            files.map((file) => {
+                console.log(`${file.name} (${file.id})`);
+            });
+        } else {
+            console.log('No files found.');
+        }
+        res.send(files);
+    });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Started ${PORT}`));
