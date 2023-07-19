73be3e9586785ed99e3c096456a734dbb99b3a55
dagmara <dagmara.gabriela.stach@gmail.com>
2023-07-19 11:21:14 +0200

    Google Cloud Platform - Access Google Drive Using a Node Server

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ import fs from "fs";
 import formidable from'formidable';
 import credentials from './credentials.json'assert { type: "json" };
 
+/*
+
+"https://www.youtube.com/watch?v=cQAbGqoZmBM";
+"https://github.com/pyrecyter/OAuth/blob/master/index.js"
+*/
 
 const client_id = credentials.web.client_id;
 const client_secret = credentials.web.client_secret;
@@ -22,6 +27,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req,res)=>res.send('API Running'))
+
+app.get('/getAuthURL', (req, res) => {
+    const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPE,
+        prompt: 'force'
+    });
+    console.log(authUrl);
+    return res.send(authUrl);
+});
+
+app.post('/getToken', (req, res) => {
+    if (req.body.code == null) return res.status(400).send('MALINOWY KRÓL ');
+    oAuth2Client.getToken(req.body.code, (err, token) => {
+        if (err) {
+            console.error('Error retrieving access token', err);
+            return res.status(400).send('Error retrieving access token');
+        }
+        res.send(token);
+    });
+});
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server Started ${PORT}`));
