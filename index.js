cb00d0c7b6310e88f8672b7dbbb7b84ee7163e4f
dagmara <dagmara.gabriela.stach@gmail.com>
2023-07-26 22:04:46 +0200

    clear

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ import express from'express';
 const app = express();
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import {drive_v3, google} from 'googleapis';
-import multer from 'multer';
-import fs from "fs";
-import formidable from'formidable';
+import {google} from 'googleapis';
 import credentials from './credentials.json'assert { type: "json" };
 const client_id = credentials.web.client_id;
 const client_secret = credentials.web.client_secret;
@@ -13,43 +10,39 @@ const redirect_uris = credentials.web.redirect_uris;
 const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 let accessToken;
 let tree = {}
+const APIkey = "AIzaSyDJLPV-CFbk1Xjd_M5Qyr4olBaDD-VvGNQ"
 
 const SCOPE = ['https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/drive.file']
-
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server Started ${PORT}`));
-
-
 app.get('/', (req,res)=>res.send('API Running'))
 
-app.get('/getAuthURL', (req, res) => {
+export const authURL = (req,res) =>{
     const authUrl = oAuth2Client.generateAuthUrl({
         access_type: 'offline',
         scope: SCOPE,
         prompt:'consent'
     });
-    console.log(authUrl);
-    return res.send(authUrl);
-});
 
-app.post('/getToken', (req, res) => {
+    res.send(authUrl);
+}
+
+
+export const getToken = (req,res) => {
     if (req.body.code == null) return res.status(400).send('ERROR - lack of token in request body');
-    oAuth2Client.getToken(req.body.code, (err, token) => {
+    oAuth2Client.getToken("4/0AZEOvhVthLOIwu_QclfR0wByIE7NU8rZPbPY7BI3JXuOsAvEFAMhcBSmHALsY3aGsOl_xA", (err, token) => {
         if (err) {
             console.error('Error retrieving access token', err);
             return res.status(400).send('Error retrieving access token');
         }
+/*
         accessToken = {token}
-        res.send(accessToken);
-
+*/
+        res.send(token);
     });
-});
+}
 
 app.post('/getUserInfo', (req, res) => {
     if (req.body.token == null) return res.status(400).send('Token not found');
--- server.js
+++ server.js
@@ -3,12 +3,17 @@ import got from 'got';
 import express from 'express'
 import enterFolder from "./controllers/enterFolder.js"
 import uploadTree from "./controllers/uploadTree.js"
+import {authURL, getToken} from "./index.js"
+
 
 const app = express()
  app.use(cors())
  app.get("/enterFolder", enterFolder);
  app.get("/getFolderTree", uploadTree);
  app.get("/getFolderTreeA", (req,res)=>res.send('API Running'))
+ app.get('/getAuthURL', authURL );
+app.post('/getToken', getToken);
+
 app.get('/a',(req, res) => {
 
     let pdfurl2 = "https://www.wm.edu/as/programs/healthy_beginnings/files/additional_info/guide-to-firsts_first-time-clipping-babys-fingernails.pdf"
@@ -18,9 +23,10 @@ app.get('/a',(req, res) => {
     let pdfurl3 = "https://media.tghn.org/articles/newbornsize.pdf"*/
     res.header(`Access-Control-Allow-Origin`, `*`);
     got.stream(pdfurl6).pipe(res);
-})
+});
 
-const PORT = process.env.PORT || 3005;
+
+const PORT = process.env.PORT || 5000;
  console.log('port to start: ', PORT)
 app.listen(PORT, () => console.log(`Server Started ${PORT}`));
 
